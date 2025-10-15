40c3595f4faf6a26eca441c31c57438b3a2f5a7f
Ademir Stefano Santos Vasquez <I202319028@cibertec.edu.pe>
2025-10-14 21:24:01 -0500

    Creacion

---
+++ src/main/java/com/T2/LP2_T2_SANTOS_ADEMIR/Lp2T2SantosAdemirApplication.java
@@ -0,0 +1,13 @@
+package com.T2.LP2_T2_SANTOS_ADEMIR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2T2SantosAdemirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2T2SantosAdemirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/T2/LP2_T2_SANTOS_ADEMIR/Lp2T2SantosAdemirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.T2.LP2_T2_SANTOS_ADEMIR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2T2SantosAdemirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
